e222a96242a684a5adb9a37a4e61643ba9057e52
William Noah <itzwilliamnoah@gmail.com>
2026-06-01 16:35:10 +0100

    Add message schema for MongoDB using Mongoose

--- backend/models/message.js
+++ backend/models/message.js
@@ -1,1 +1,14 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    subject: { type: String },
+    message: { type: String, required: true }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Message", messageSchema);
 
