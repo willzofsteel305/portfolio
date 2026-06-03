1d91592cc6391e95e78cdb2a7c526bf4c43677d1
William Noah <itzwilliamnoah@gmail.com>
2026-06-03 14:55:20 +0100

    Fix import statement for User model

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+import user from "../models/User.js";
 
 const router = express.Router();
 
